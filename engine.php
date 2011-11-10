4fd0f5b171dff0efafbd9a2830317d6df75dd915
Buganini Q <buganini@gmail.com>
2011-11-11 02:11:45 +0800

    first workable version

---
+++ engine.php
@@ -0,0 +1,13 @@
+<?php
+if($_REQUEST['action']=='DECOMP'){
+	$p=bsdconv_create("utf-8:zh_decomp:split:bsdconv_keyword,bsdconv");
+	$s=bsdconv($p, $_POST['data']);
+	bsdconv_destroy($p);
+	echo json_encode(explode(',',trim($s,',')));
+}elseif($_REQUEST['action']=='COMP'){
+	$p=bsdconv_create("bsdconv:zh_comp:utf-8");
+	$s=bsdconv($p, $_POST['data']);
+	bsdconv_destroy($p);
+	echo json_encode($s);
+}
+?>
